6b693274f16b2b14e3c4fa05b26125e4cd5a943d
Isuruhewawasam <isuruhewawasam@gmail.com>
2021-06-27 20:22:41 +0530

    change setting.py

--- My_blog/settings.py
+++ My_blog/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATICFILES_DIR = (os.path.join(BASE_DIR,'static'))
+STATICROOT= (os.path.join(BASE_DIR,'static'))
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
